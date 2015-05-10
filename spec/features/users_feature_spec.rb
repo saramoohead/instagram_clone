76f3493abbfb1349f94a2c01e49a56ca639b2eb2
Sara OC <saramoo@hotmail.com>
2015-05-10 22:41:40 +0100

    sign in and out links conditional

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -13,4 +13,26 @@ feature 'users' do
       expect(page).not_to have_link('Sign out')
     end
   end
+
+  context 'when user is signed in' do
+    before do
+      visit('/')
+      click_link('Sign up')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      fill_in('Password confirmation', with: 'testtest')
+      click_button('Sign up')
+    end
+
+    it 'user sees a sign out link' do
+      visit('/')
+      expect(page).to have_link('Sign out')
+    end
+
+    it 'and does not see a sign in or sign up link' do
+      visit('/')
+      expect(page).not_to have_link('Sign in')
+      expect(page).not_to have_link('Sign up')
+    end
+  end
 end
